9751fa3b9584810efc623a17c54485a8c2da55b4
JaejuGom <inkcloud365@gmail.com>
2020-11-14 18:57:11 +0900

    ReadMe Change

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HellOSpring
+BCSD AOP 추가 실습입니다.
+참고목록
+- [Model, ModelAndView 사용법] (https://hongku.tistory.com/116)
+- [@PathVariable] 사용하기 (https://hongku.tistory.com/121)
--- src/main/java/homework/aop/controller/SimpleLoginController.java
+++ src/main/java/homework/aop/controller/SimpleLoginController.java
@@ -2,8 +2,6 @@ package homework.aop.controller;
 
 import homework.aop.service.SimpleLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
